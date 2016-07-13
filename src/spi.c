1d35908ab1c371e35f618df83f4f17ec72ebcd0a
Jacob Pålsson <jacpa975@student.liu.se>
2016-07-13 20:54:33 +0200

    Update spi.c

--- src/spi.c
+++ src/spi.c
@@ -12,11 +12,25 @@
 
 void Write(char* data, char length) {
     
-
+    spi_msg.rx_buf = 0; // Block SPI from reading anything.
+    spi_msg.tx_buf = (unsigned long)data;
+    spi_msg.len = length;
+    mraa_gpio_write(gpio, 0);
+          if (ioctl(dev->devfd, SPI_IOC_MESSAGE(1), &spi_msg) < 0) {
+    }
+    mraa_gpio_write(gpio, 1);
 }
 char* WriteRead(char* data, char length) {
-return 0;
+    spi_msg.rx_buf = (unsigned long) spi_rx; // Block SPI from reading anything.
+    spi_msg.tx_buf = (unsigned long) data;
+    spi_msg.len = length;
+    mraa_gpio_write(gpio, 0);
+    if (ioctl(dev->devfd, SPI_IOC_MESSAGE(1), &spi_msg) < 0) {
+    }
+    mraa_gpio_write(gpio, 1);
+    return spi_rx;
 }
 void initGPIO(){
-
+    gpio = mraa_gpio_init_raw(1);
+    mraa_gpio_dir(gpio, MRAA_GPIO_OUT);
 }
