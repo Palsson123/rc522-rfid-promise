ffdcb0d45b0c35f5d3eb23f972bccc3f01a02798
Jacob Pålsson <jacpa975@student.liu.se>
2016-07-16 19:36:53 +0200

    Update spi.c

--- src/spi.c
+++ src/spi.c
@@ -16,8 +16,7 @@ void Write(char* data, char length) {
     spi_msg.tx_buf = (unsigned long)data;
     spi_msg.len = length;
     mraa_gpio_write(gpio, 0);
-          if (ioctl(dev->devfd, SPI_IOC_MESSAGE(1), &spi_msg) < 0) {
-    }
+     
     mraa_gpio_write(gpio, 1);
 }
 char WriteRead(char* data, char length) {
