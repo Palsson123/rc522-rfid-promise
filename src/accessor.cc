b5d2beab6ebae9cbbde1ae0474452bd514dd190f
Jacob Pålsson <jacpa975@student.liu.se>
2016-07-16 18:57:15 +0200

    Update accessor.cc

--- src/accessor.cc
+++ src/accessor.cc
@@ -31,8 +31,54 @@ void RunCallback(const FunctionCallbackInfo<Value>& args) {
         InitRc522();
 
         for (;; ) {
-                
-               
+                statusRfidReader = find_tag(&CType);
+                if (statusRfidReader == TAG_NOTAG) {
+
+                        // The status that no tag is found is sometimes set even when a tag is within reach of the tag reader
+                        // to prevent that the reset is performed the no tag event has to take place multiple times (ger: entrprellen)
+                        if (noTagFoundCount > 2) {
+                                // Sets the content of the array 'rfidChipSerialNumberRecentlyDetected' back to zero
+                                memset(&rfidChipSerialNumberRecentlyDetected[0], 0, sizeof(rfidChipSerialNumberRecentlyDetected));
+                                noTagFoundCount = 0;
+                        }
+                        else {
+                                noTagFoundCount++;
+                        }
+
+                        usleep(200000);
+                        continue;
+                } else if (statusRfidReader != TAG_OK && statusRfidReader != TAG_COLLISION) {
+                        continue;
+                }
+
+                if (select_tag_sn(serialNumber,&serialNumberLength) != TAG_OK) {
+                        continue;
+                }
+
+                // Is a successful detected, the counter will be set to zero
+                noTagFoundCount = 0;
+
+                p=rfidChipSerialNumber;
+                for (loopCounter = 0; loopCounter < serialNumberLength; loopCounter++) {
+                        sprintf(p,"%02x", serialNumber[loopCounter]);
+                        p+=2;
+                }
+
+                // Only when the serial number of the currently detected tag differs from the
+                // recently detected tag the callback will be executed with the serial number
+                if(strcmp(rfidChipSerialNumberRecentlyDetected, rfidChipSerialNumber) != 0)
+                {
+                        Local<Value> argv[argc] = {
+                                Local<Value>::New(isolate,String::NewFromUtf8(isolate,&rfidChipSerialNumber[0]))
+                        };
+                        callback->Call(isolate->GetCurrentContext()->Global(), argc, argv);
+                }
+
+                // Preserves the current detected serial number, so that it can be used
+                // for future evaluations
+                strcpy(rfidChipSerialNumberRecentlyDetected, rfidChipSerialNumber);
+
+                *(p++)=0;
         }
 
 }
