fb4d2b73b219e9e1c3cbf012c935766314680d66
Jacob Pålsson <jaaacke@gmail.com>
2016-07-18 04:42:27 -0700

    Now only tries to read a tag once and then returns the result with corresponding error code

--- src/accessor.cc
+++ src/accessor.cc
@@ -23,88 +23,80 @@ int loopCounter;
 using namespace v8;
 
 void RunCallback(const FunctionCallbackInfo<Value>& args) {
-        Isolate* isolate=Isolate::GetCurrent();
-        HandleScope scope(isolate);
-	int sleepTime = 200000; 
-	int timeout = args[1]->Int32Value(); //milliseconds 
-	int counter = 0;
-        Local<Function> callback = Local<Function>::Cast(args[0]);
-        const unsigned argc = 1;
+	Isolate* isolate=Isolate::GetCurrent();
+	HandleScope scope(isolate);
+	Local<Function> callback = Local<Function>::Cast(args[0]);
+	const unsigned argc = 2;
 
-        InitRc522();
+	InitRc522();
 
-        for (;; ) {
-		if(counter >=timeout)
+	for (;; ) {
+		statusRfidReader = find_tag(&CType);
+		if (statusRfidReader == TAG_NOTAG) {
+
+			// The status that no tag is found is sometimes set even when a tag is within reach of the tag reader
+			// to prevent that the reset is performed the no tag event has to take place multiple times (ger: entrprellen)
+			if (noTagFoundCount > 2) {
+				// Sets the content of the array 'rfidChipSerialNumberRecentlyDetected' back to zero
+				memset(&rfidChipSerialNumberRecentlyDetected[0], 0, sizeof(rfidChipSerialNumberRecentlyDetected));
+				noTagFoundCount = 0;
+			}
+			else {
+				noTagFoundCount++;
+			}
+			Local<Value> argv[argc] = {
+				Local<Value>::New(isolate,Integer::New(isolate,1)),
+				Local<Value>::New(isolate,String::NewFromUtf8(isolate,"undefined"))
+			};
+			callback->Call(isolate->GetCurrentContext()->Global(), argc, argv);
+			break;
+
+		} else if (statusRfidReader != TAG_OK && statusRfidReader != TAG_COLLISION) {
+			continue;
+		}
+
+		if (select_tag_sn(serialNumber,&serialNumberLength) != TAG_OK) {
+			continue;
+		}
+		// Is a successful detected, the counter will be set to zero
+		noTagFoundCount = 0;
+
+		p=rfidChipSerialNumber;
+		for (loopCounter = 0; loopCounter < serialNumberLength; loopCounter++) {
+			sprintf(p,"%02x", serialNumber[loopCounter]);
+			p+=2;
+		}
+
+		// Only when the serial number of the currently detected tag differs from the
+		// recently detected tag the callback will be executed with the serial number
+		if(strcmp(rfidChipSerialNumberRecentlyDetected, rfidChipSerialNumber) != 0)
 		{
 			Local<Value> argv[argc] = {
-                      	  Local<Value>::New(isolate,String::NewFromUtf8(isolate,"undefined"))
-                        };
-                        callback->Call(isolate->GetCurrentContext()->Global(), argc, argv);
-			counter=0;
+				Local<Value>::New(isolate,Integer::New(isolate,0)),
+				Local<Value>::New(isolate,String::NewFromUtf8(isolate,&rfidChipSerialNumber[0]))
+			};
+			callback->Call(isolate->GetCurrentContext()->Global(), argc, argv);
 			break;
 		}
-                statusRfidReader = find_tag(&CType);
-                if (statusRfidReader == TAG_NOTAG) {
 
-                        // The status that no tag is found is sometimes set even when a tag is within reach of the tag reader
-                        // to prevent that the reset is performed the no tag event has to take place multiple times (ger: entrprellen)
-                        if (noTagFoundCount > 2) {
-                                // Sets the content of the array 'rfidChipSerialNumberRecentlyDetected' back to zero
-                                memset(&rfidChipSerialNumberRecentlyDetected[0], 0, sizeof(rfidChipSerialNumberRecentlyDetected));
-                                noTagFoundCount = 0;
-                        }
-                        else {
-                                noTagFoundCount++;
-                        }
-			
-                        usleep(sleepTime);
-			counter += sleepTime/1000; //Convert sleeptime to milliseconds	
-                        continue;
-                } else if (statusRfidReader != TAG_OK && statusRfidReader != TAG_COLLISION) {
-                        continue;
-                }
+		// Preserves the current detected serial number, so that it can be used
+		// for future evaluations
+		strcpy(rfidChipSerialNumberRecentlyDetected, rfidChipSerialNumber);
 
-                if (select_tag_sn(serialNumber,&serialNumberLength) != TAG_OK) {
-                        continue;
-                }
-                // Is a successful detected, the counter will be set to zero
-                noTagFoundCount = 0;
-
-                p=rfidChipSerialNumber;
-                for (loopCounter = 0; loopCounter < serialNumberLength; loopCounter++) {
-                        sprintf(p,"%02x", serialNumber[loopCounter]);
-                        p+=2;
-                }
-
-                // Only when the serial number of the currently detected tag differs from the
-                // recently detected tag the callback will be executed with the serial number
-                if(strcmp(rfidChipSerialNumberRecentlyDetected, rfidChipSerialNumber) != 0)
-                {
-                        Local<Value> argv[argc] = {
-                                Local<Value>::New(isolate,String::NewFromUtf8(isolate,&rfidChipSerialNumber[0]))
-                        };
-                        callback->Call(isolate->GetCurrentContext()->Global(), argc, argv);
-			break;
-                }
-
-                // Preserves the current detected serial number, so that it can be used
-                // for future evaluations
-                strcpy(rfidChipSerialNumberRecentlyDetected, rfidChipSerialNumber);
-
-                *(p++)=0;
-        }
+		*(p++)=0;
+	}
 
 }
 
 void Init(Handle<Object> exports, Handle<Object> module) {
-        initRfidReader();
-        NODE_SET_METHOD(module,"exports",RunCallback);
+	initRfidReader();
+	NODE_SET_METHOD(module,"exports",RunCallback);
 }
 
 uint8_t initRfidReader(void) {
 
-        initGPIO();
-        return 0;
+	initGPIO();
+	return 0;
 }
 
 NODE_MODULE(rc522, Init)
